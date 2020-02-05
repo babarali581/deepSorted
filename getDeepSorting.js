f538cdca7bc3fab9d0f5d2c32b0e5e6cfd608f11
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-02-05 19:40:51 +0500

    depSort

--- getDeepSorting.js
+++ getDeepSorting.js
@@ -1,50 +1,54 @@
 const getSortNestedArray = require("./helper/getSortNestedArray")
 const getSortedObjectKeys = require('./helper/getSortObject')
 
-
 function deepSort(arr) {
-    if(Array.isArray(arr)){
-    let finalResp = arr.reduce((acc, val) => {
+    try {
+        if (Array.isArray(arr)) {
+            let finalResp = arr.reduce((acc, val) => {
 
-        let eachVal = typeof val === "string"
-        ? "String"
-        : typeof val === "number"
-            ? "Number"
-            : Array.isArray(val)
-                ? "Array"
-                : "Object"
+                let eachVal = typeof val === "string"
+                    ? "String"
+                    : typeof val === "number"
+                        ? "Number"
+                        : Array.isArray(val)
+                            ? "Array"
+                            : "Object"
 
-        /* In case of string */
-        if (eachVal === "String" || eachVal === "Number") {
-            console.log("inside");
-             return acc.concat(val)
-            // return !acc.includes(val)
-            //     ? acc.concat(val)
-            //     : acc
-        } else if (eachVal === "Object") {
+                /* In case of string */
+                if (eachVal === "String" || eachVal === "Number") {
+                    console.log("inside");
+                    return acc.concat(val)
+                    // return !acc.includes(val)     ? acc.concat(val)     : acc
+                } else if (eachVal === "Object") {
 
-            val = getSortedObjectKeys(val)
-            return !acc.includes(val)
-                ? acc.concat(val)
-                : acc
+                    val = getSortedObjectKeys(val)
+                    return !acc.includes(val)
+                        ? acc.concat(val)
+                        : acc
 
-        } else if (eachVal === "Array") {
+                } else if (eachVal === "Array") {
 
-            val = getSortNestedArray(val)
+                    val = getSortNestedArray(val)
 
-            return [...acc , val]
+                    return [
+                        ...acc,
+                        val
+                    ]
 
+                }
+
+            }, [])
+            return finalResp.sort()
+        } else if (typeof arr === "object") {
+            return getSortedObjectKeys(arr)
+        } else {
+            return new Error("You should provide array or object")
         }
 
-    }, [])
-    return finalResp.sort()
- }else if(typeof arr === "object"){
-     console.log("inside object");
-     return getSortedObjectKeys(arr)
- }else{
-     return new Error("You should provide array or object")
- }
-}
+    } catch (err) {
+        return new Error(err)
 
+    }
+}
 
 module.exports = deepSort
